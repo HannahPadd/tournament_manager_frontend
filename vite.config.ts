198d15200b4243c89a42da24d2532e699a560352
Hannah Lynn Lindrob <hannahlindrob@outlook.com>
2026-01-08 13:59:48 +0100

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,6 @@ export default defineConfig({
   server: {
     port: 5173,
     host: "0.0.0.0",
-    allowedHosts: "manager.itgeurocup.com"
+    allowedHosts: "http://manager.itgeurocup.com"
   },
 });
